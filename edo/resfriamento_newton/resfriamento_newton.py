a77c1aa930f7284d15f5c365a445cc2e829be52b
Kelly Steffany <kellysteffanys@gmail.com>
2021-06-16 18:44:48 -0300

    resfriamento newton

---
+++ edo/resfriamento_newton/resfriamento_newton.py
@@ -0,0 +1,89 @@
+from math import exp,log
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Valores iniciais
+T0 = 80                         # Temperatura inicial do corpo
+T1 = 40                         # Temperatura apos 5 minutos
+Ta = 0                          # Temperatura do ambiente
+k = log(T1/T0)/5                # Constante determinada experimentalmente
+
+a = 0                           # Tempo inicial
+b = 30                          # Tempo final
+m = 20                          # Numero de subintervalos
+h = (b-a)/m                     # Passo
+x = np.linspace(a, b, m)
+
+def funcao(x, y):
+    f = k*exp(k*x)*(T0 - Ta)    # Derivada da funcao de temperatura do corpo
+    return f
+    
+# Metodo de Euler
+def euler():
+    y = np.zeros([m])
+    y[0] = T0
+    for i in range(0, m-1):
+        y[i+1] = y[i] + h*funcao(x[i], y[i])
+    return y
+
+# Metodo de Euler Modificado
+def euler_modificado():
+    y = np.zeros([m])
+    y[0] = T0
+    for i in range(0, m-1):
+        f = funcao(x[i], y[i])
+        y[i+1] = y[i] + h*funcao(x[i] + 0.5*h, y[i] + 0.5*h*f)
+    return y
+
+# Metodo de Euler Melhorado
+def euler_melhorado():
+    y = np.zeros([m])
+    y[0] = T0
+    for i in range(0, m-1):
+        f = funcao(x[i], y[i])
+        ff = funcao(x[i] + h, y[i] + h*f)
+        y[i+1] = y[i] + 0.5*h*(f + ff)
+    return y
+
+# Metodo de Runge-Kutta de 4a ordem
+def runge_kutta():
+    y = np.zeros([m])
+    y[0] = T0
+    for i in range(0, m-1):
+        k1 = funcao(x[i], y[i])
+        k2 = funcao(x[i] + 0.5*h, y[i] + 0.5*h*k1)
+        k3 = funcao(x[i] + 0.5*h, y[i] + 0.5*h*k2)
+        k4 = funcao(x[i] + h, y[i] + h*k3)
+        y[i+1] = y[i] + (h/6)*(k1 + 2*k2 + 2*k3 + k4)
+    return y
+
+# Solucao analitica
+def analitica():
+    y = np.zeros([m])
+    for i in range(0, m):
+        y[i] = (T0-Ta)*exp(k*x[i]) + Ta
+    return y
+
+y_e = euler()
+y_emod = euler_modificado()
+y_emel = euler_melhorado()
+y_rk = runge_kutta()
+y_a = analitica()
+
+# Tabela de resultados
+print("Lei de Newton do Resfriamento para agua em um ambiente de 0 graus Celsius")
+print("x\t Euler\t\t Euler Mod\t Euler Mel\t Runge-Kutta\t Analitica")
+for i in range(m):
+	print(format(x[i],'.1f'),"\t",format(y_e[i],'.7f'),"\t",format(y_emod[i],'.7f'),"\t",format(y_emel[i],'.7f'),"\t",format(y_rk[i],'.7f'),"\t",format(y_a[i],'.7f'))
+
+# Plot das curvas
+plt.plot(x, y_e, label="Euler")
+plt.plot(x, y_emod, label="Euler Modificado")
+plt.plot(x, y_emel, label="Euler Melhorado")
+plt.plot(x, y_rk, label="Runge Kutta")
+plt.plot(x, y_a, label="Analitica")
+plt.title("Resfriamento da agua em um ambiente de 0 graus Celsius")
+plt.xlabel("Tempo (minutos)")
+plt.ylabel("Temperatura (graus Celsius)")
+plt.legend()
+plt.show()
